a59424b632f6155bb0144d5a9729eb791727730a
usumaro <usuviich@gmail.com>
2022-08-26 15:44:01 +0900

    課題3-3

---
+++ kadai3-3.php
@@ -0,0 +1,19 @@
+<?php
+
+$number1=10;
+$number2=0;
+
+
+switch($number2)
+{
+    case 0:
+        print "0で割り算はできません";
+        break;
+  
+        default:
+        print $number1/$number2;
+        break;
+
+};
+
+?>
